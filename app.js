449eb7b640eb6f4494d0dec01bd8eaf512ddd4ed
Mike Back <mikeback75@hotmail.com>
2020-07-14 00:42:22 +0000

    update app.js to fix r10 error - dynamic port config

--- app.js
+++ app.js
@@ -7,4 +7,10 @@ app.get("/", (req,res)=>{res.render("home");});
 app.get("/stuff", (req,res)=>{res.render("stuff");});
 
 
-app.listen(3000, process.env.IP, ()=>{console.log("Server is running");});
+// app.listen(3000, process.env.IP, ()=>{console.log("Server is running");});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
+});
+
